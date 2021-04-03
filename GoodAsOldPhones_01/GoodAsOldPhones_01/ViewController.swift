2bc7bf8a061d25d34c5961a83ffba13f55176d4a
taeroi <fkrkdlsgk@gmail.com>
2021-04-03 23:32:24 +0900

    project01_UIKit, Foundation Tutorial

---
+++ GoodAsOldPhones_01/GoodAsOldPhones_01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodAsOldPhones_01
+//
+//  Created by 태로고침 on 2021/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
