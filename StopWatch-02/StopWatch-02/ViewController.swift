7686212d23c18a186412c44c44512e5eb67c35f2
taeroi <fkrkdlsgk@gmail.com>
2021-04-04 17:33:39 +0900

    Set project02-UIKit, Foundation Tutorial

---
+++ StopWatch-02/StopWatch-02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StopWatch-02
+//
+//  Created by 태로고침 on 2021/04/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StopWatch-02/StopWatch-02Tests/StopWatch_02Tests.swift
@@ -0,0 +1,33 @@
+//
+//  StopWatch_02Tests.swift
+//  StopWatch-02Tests
+//
+//  Created by 태로고침 on 2021/04/04.
+//
+
+import XCTest
+@testable import StopWatch_02
+
+class StopWatch_02Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
